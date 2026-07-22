fecd26816dee1b2d79b311361d939567adcb4138
ARCHIT AGARWAL <architagarwal2007@ARCHITs-MACBOOK-AIR.local>
2026-07-22 23:58:17 +0530

    changes

--- src/components/landing/KnowMore.jsx
+++ src/components/landing/KnowMore.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import AboutUs from "../../components/landing/AboutUs";
 import ContactUs from "../../components/landing/ContactUs";
 import styles from "../../styles/landing/KnowMore.module.css";
-import KnowMoreBg from "../../assets/images/KnowMore/KnowMoreBg.svg";
-import FooterStrip from "../../assets/images/KnowMore/footerStrip.svg";
+import KnowMoreBg from "../../assets/images/knowmore/KnowMoreBg.svg";
+import FooterStrip from "../../assets/images/knowmore/footerStrip.svg";
 const KnowMore = () => {
   return (
     <div className={styles.KnowMoreWrapper} style={{backgroundImage:`url(${KnowMoreBg})`}}> 
